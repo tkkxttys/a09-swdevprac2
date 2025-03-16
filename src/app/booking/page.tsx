7b169f60fe71a7b99a2426cd9c3a348b6ab9a1e0
tkkxttys <kittycatty.2548@gmail.com>
2025-03-17 00:13:51 +0700

    add all feature

--- src/app/booking/page.tsx
+++ src/app/booking/page.tsx
@@ -1,9 +1,21 @@
 import LocationDateReserve from "@/components/DateReserve";
 import { TextField, Select, MenuItem, Button, FormControl, InputLabel } from "@mui/material";
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
+import getUserProfile from "@/libs/getUserProfile"
 
-export default function Reservations () {
-    return (
-        <main className="w-[100%] flex flex-col items-center space-y-4">
+export default async function Reservations () {
+
+    const session = await getServerSession(authOptions)
+    if(!session || !session.user.token) return null
+
+    const profile = await getUserProfile(session.user.token)
+    var createAt = new Date(profile.data.createAt)
+
+    return(
+        <div>
+
+        <div className="w-[100%] flex flex-col items-center space-y-4">
             <div className="text-xl font-medium">New Reservation</div>
 
             <TextField id="Name-Lastname" name="Name-Lastname" label="Name-Lastname" variant="standard" />
@@ -19,6 +31,16 @@ export default function Reservations () {
             </Button>
 
 
-        </main>
-    );
+        </div>
+        <div className="bg-slate-100 m-5 p-5">
+            <div className="text-2xl">{profile.data.name}</div>
+            <table className="table-auto border-separate border-spacing-2"><tbody>
+                <tr><td>Email</td><td>{profile.data.name}</td></tr>
+                <tr><td>Tel.</td><td>{profile.data.tel}</td></tr>
+                <tr><td>Member Since</td><td>{createAt.toString()}</td></tr>
+            </tbody></table>
+        </div>
+
+        </div>
+    )
 }
