ac07534eb017ab48bf2da839d6d8351211f8d391
tkkxttys <kittycatty.2548@gmail.com>
2025-03-17 00:29:44 +0700

    edit link

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,6 +1,6 @@
 export default async function getUserProfile(token: string) {
 
-    const response = await fetch("http://localhost:3000/api/v1/auth/me", {
+    const response = await fetch(" https://a08-venue-explorer-backend.vercel.app/api/v1/auth/me", {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`,
