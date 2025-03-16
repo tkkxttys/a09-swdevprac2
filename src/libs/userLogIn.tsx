cb337d85c1898a2dad7ac288da547843f4353a07
tkkxttys <kittycatty.2548@gmail.com>
2025-03-17 00:22:22 +0700

    edit fetch link

--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -1,6 +1,6 @@
 export default async function userLogIn(userEmail: string, userPassword: string) {
 
-    const response = await fetch("https://backend-domain/api/v1/auth/login", {
+    const response = await fetch("http://localhost:3000/api/v1/auth/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
