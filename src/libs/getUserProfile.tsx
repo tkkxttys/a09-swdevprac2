9713acd096d2ad7ebc1ba34c00d23e852e3684dc
tkkxttys <kittycatty.2548@gmail.com>
2025-03-17 00:34:15 +0700

    link a08-3

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,6 +1,6 @@
 export default async function getUserProfile(token: string) {
 
-    const response = await fetch(" https://a08-venue-explorer-backend.vercel.app/api/v1/auth/me", {
+    const response = await fetch("https://a08-venue-explorer-backend-3.vercel.app/api/v1/auth/me", {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`,
--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -1,6 +1,6 @@
 export default async function userLogIn(userEmail: string, userPassword: string) {
 
-    const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/auth/login", {
+    const response = await fetch("https://a08-venue-explorer-backend-3.vercel.app/api/v1/auth/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
